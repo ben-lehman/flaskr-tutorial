4d4826dafc32b71691d558b8ddef75f9ad593c01
Ben Lehman <ben.a.lehman@gmail.com>
2019-05-27 12:51:16 -0400

    Added in tests

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -17,9 +17,9 @@ def register():
         error = None
 
         if not username:
-            error = 'Username is required'
+            error = 'Username is required.'
         elif not password:
-            error = 'Password is required'
+            error = 'Password is required.'
         elif db.execute(
             'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
@@ -49,7 +49,7 @@ def login():
         ).fetchone()
 
         if user is None:
-            error = 'Incorrect username'
+            error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
